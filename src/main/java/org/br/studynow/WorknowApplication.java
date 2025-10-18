feace4c66a4064a912b8591b402079e4c777ae23
gabizinha12 <gaibou_lima@hotmail.com>
2025-10-17 22:33:37 -0300

    feat: Inicio do desafio

---
+++ src/main/java/org/br/studynow/WorknowApplication.java
@@ -0,0 +1,14 @@
+package org.br.studynow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorknowApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WorknowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/br/studynow/WorknowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.br.studynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorknowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
